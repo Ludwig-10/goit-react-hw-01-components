2a7d371359b30fb82599ba57c8bce87f9c397022
Denys Karakoi <denys.karakoi@gmail.com>
2021-04-21 21:39:57 +0200

    formatter cod

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
 import React from 'react';
-import Container from './components/container/Container';
-import Profile from './components/profile/Profile';
+import Container from './components/container/container';
+import Profile from './components/profile/profile';
 import user from './data/user.json';
-import Statistics from './components/statistics/Statistics';
+import Statistics from './components/statistics/statistics';
 import statisticalData from './data/statistical-data.json';
-import FriendList from './components/FriendList/friends';
+import FriendList from './components/friendList/friends';
 import friends from './data/friends.json';
 import transactions from './data/transactions.json';
-import TransactionHistory from './components/TransactionHistory/TransactionHistory';
- const App = () => {
-     return (
-      <Container>
-        <Profile
-          name={user.name}
-          tag={user.tag}
-          location={user.location}
-          avatar={user.avatar}
-          stats={user.stats}
-        />
-        <Statistics stats={statisticalData} />
-        <FriendList friends={friends} />
-        <TransactionHistory items={transactions} />
-        </Container>
-     )
- }
- export default App;
+import TransactionHistory from './components/transactionHistory/transactionHistory';
+const App = () => {
+  return (
+    <Container>
+      <Profile
+        name={user.name}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics stats={statisticalData} />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
+    </Container>
+  );
+};
+export default App;
--- src/components/statistics/Statistics.js
+++ src/components/statistics/Statistics.js
@@ -2,33 +2,27 @@ import React from 'react';
 import styles from './statistic.module.css';
 import PropTypes from 'prop-types';
 
-const Statistics = ({ title, stats }) =>{
-    return (
-        <section className={styles.statistics}>
-  <h2 className={styles.title}>{title}</h2>
+const Statistics = ({ title, stats }) => {
+  return (
+    <section className={styles.statistics}>
+      {title && <h2 className={styles.title}></h2>}
 
-  <ul className={styles.statList}>
-    {stats.map(({ id, label, percentage }) => (
-    <li className={styles.item}
-            key={id}
-          >
-        <span className={styles.label}>{label}</span>
-        <span className={styles.percentage}>{percentage}%</span>
-    </li>
+      <ul className={styles.statList}>
+        {stats.map(({ id, label, percentage }) => (
+          <li className={styles.item} key={id}>
+            <span className={styles.label}>{label}</span>
+            <span className={styles.percentage}>{percentage}%</span>
+          </li>
         ))}
-    </ul>
-</section>
-    )
-}
+      </ul>
+    </section>
+  );
+};
 Statistics.defaultProps = {
-    title: 'Upload stats',
-  };
+  title: 'Upload stats',
+};
 Statistics.propTypes = {
-    title: PropTypes.string,
-    stats: PropTypes.array.isRequired,
-  };
+  title: PropTypes.string,
+  stats: PropTypes.array.isRequired,
+};
 export default Statistics;
-
-
-
-
